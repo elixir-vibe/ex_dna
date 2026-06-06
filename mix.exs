c405ffbd6c8aefb45e4dd07067643aad853ddc0e
Danila Poyarkov <dev@dannote.net>
2026-06-06 16:59:39 +0300

    Use Elixir 1.20 by default

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ExDNA.MixProject do
     [
       app: :ex_dna,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "ExDNA",
